39f62deb741c545b9aac1fa2ef47a38807597bbc
pahanez <pahanez@gmail.com>
2014-07-22 18:39:21 +0200

    test

--- FlagbarSample/src/com/pahanez/flagbar/sample/MainActivity.java
+++ FlagbarSample/src/com/pahanez/flagbar/sample/MainActivity.java
@@ -1,23 +1,23 @@
 package com.pahanez.flagbar.sample;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
 
+@SuppressLint("NewApi")
 public class MainActivity extends ActionBarActivity {
 
 	private int mProgress = 0;
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) { 
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
